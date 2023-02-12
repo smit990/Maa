13411b5b82a464a77fb38c704b7adbfdb9f8cc10
Smit <124628708+smit990@users.noreply.github.com>
2023-02-12 01:00:51 -0500

    Update JavaApplication7.java

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -12,7 +12,7 @@ public class JavaApplication7 {
      */
     public static void main(String[] args) {
         System.out.print("JAi MAHA KALIKA MAA");
-                System.out.print("JAi MAHA KALIKA MAA MELDI MAA");
+                System.out.print("JAi MAHA KALIKA MAA MELDI MAAA");
 
     }
 
