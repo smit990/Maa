1b8b44e97bc20ba6dd03cb1653191a226268d3b2
smit990 <smitpm5956@gmail.com>
2023-02-12 01:49:41 -0500

    2M

--- src/javaapplication7/JavaApplication7.java
+++ src/javaapplication7/JavaApplication7.java
@@ -13,6 +13,8 @@ public class JavaApplication7 {
     public static void main(String[] args) {
         System.out.print("JAi MAHA KALIKA MAA");
                 System.out.print("JAi MAHA KALIKA MAA MELDI MAAA");
+                                System.out.print("JAi MAHA KALIKA MAA MELDI MAAA BHAGVATI MAA");
+
 
     }
 
